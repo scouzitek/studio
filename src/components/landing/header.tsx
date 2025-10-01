4e550596bce9287812452f99a52f46a3a5b8e421
Aleksi Rajakoski <rajakoski.aleksi@gmail.com>
2025-10-01 20:19:37 +0000

    remplace en haut a gauche "novix AI" par "Novix"

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex items-center">
-          <span className="font-bold font-headline">Novix AI</span>
+          <span className="font-bold font-headline">Novix</span>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
